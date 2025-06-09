cd791ba108b259840a52ad26d77133e9e0d4c7f6
Android Studio <devandroid@ort>
2025-06-09 10:47:16 -0300

    1er codeo. muy relajado, poco avance

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -1,14 +1,9 @@
-import 'dotenv/config'
-
 const config = {
-    user        : process.env.DB_USER,
-    password    : process.env.DB_PASSWORD,
-    server      : process.env.DB_SERVER,
-    database    : process.env.DB_DATABASE,
-    options     : {
-        trustServerCertificate  : true,
-        trustedConnection       : true,
-    }
-}
-
+    user: 'postgres',
+    host: 'localhost',
+    database: 'tp',
+    password: 'root',
+    port: 9302,
+    };
+    
 export default config;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
-import { Express } from "express";
+//queda aprender como interactuar con una base de datos.
+//ya se construir una api ahora solo necesito q tenga una bd para pode pedirle cosas
+import express from "express";
 import cors from "cors";
 import config from './configs/db-config.js'
 import pkg from 'pg'
 
 const {Client} = pkg;
 
-const app=express();
+const app = express();
 const port = 9302;
 app.use(cors());
 app.use(express.json());
 
 app.get('/api/alumnos', async (req,res)=>{
-
+    const result= await Client.query("SELECT * FROM Alumnos");// <-- es por acá
+    console.log(result);
+    res.send(`hola ${req.params.nombre}`)
 })
 
-app.isten(port,()=>{
+app.listen(port,()=>{
     console.log(`App escuchando en puerto ${port}`);
 })
